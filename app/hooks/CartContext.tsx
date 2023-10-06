2a6f5a0b04c9cb17d2fa1fc7b17729cfa9322d7e
Michal Ciberej <michal.ciberej@gmail.com>
2023-10-06 23:15:59 +0200

    Added custom CartContext hook

---
+++ app/hooks/CartContext.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { CartedProductType } from '@/typings';
+import { useState, createContext, useContext } from 'react';
+
+type CartContextType = {
+  carted: CartedProductType[] | [];
+  setCarted: React.Dispatch<React.SetStateAction<CartedProductType[] | []>>;
+};
+
+const CartContext = createContext<CartContextType | null>(null);
+
+const CartContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [carted, setCarted] = useState([]);
+
+  return (
+    <CartContext.Provider value={{ carted, setCarted }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContextProvider;
+
+export const useCartContext = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCartContext must be used within a CartContextProvider');
+  }
+  return context;
+};
