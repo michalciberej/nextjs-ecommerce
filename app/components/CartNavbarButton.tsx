42d76e6c6d11e539ef6f58fc702ae9ade355c770
Michal Ciberej <michal.ciberej@gmail.com>
2023-10-06 23:19:46 +0200

    Added products to display and styles

--- app/components/CartNavbarButton.tsx
+++ app/components/CartNavbarButton.tsx
@@ -2,22 +2,38 @@
 
 import React, { useState } from 'react';
 import useDelayUnmount from '../hooks/UseDelayUnmount';
+import Image from 'next/image';
 import Icon from '@mdi/react';
+import Link from '@/node_modules/next/link';
+import { mdiClose, mdiCartOutline, mdiTrashCanOutline } from '@mdi/js';
+import { useCartContext } from '../hooks/CartContext';
+import { CartedProductType } from '@/typings';
 
-import { mdiClose, mdiCartOutline } from '@mdi/js';
+const bgColors = ['bg-slate-400', 'bg-blue-400', 'bg-orange-400'];
 
 const CartNavbarButton = () => {
   const [isCartOpened, setIsCartOpened] = useState(false);
+  const { carted, setCarted } = useCartContext();
   const shouldRenderCart = useDelayUnmount(isCartOpened, 500);
 
-  const handleCartToggle = () => {
-    setIsCartOpened(!isCartOpened);
+  const updateProductQuantity = (product: CartedProductType) => {
+    setCarted(
+      carted.map((productToIncrement: CartedProductType) => {
+        if (productToIncrement.id === product.id) {
+          return {
+            ...productToIncrement,
+            quantity: productToIncrement.quantity++,
+          };
+        }
+      })
+    );
   };
+
   return (
     <>
       <button
         type='button'
-        onClick={handleCartToggle}
+        onClick={() => setIsCartOpened(!isCartOpened)}
         className='flex space-x-2 items-center justify-center mr-[3vw]'>
         <Icon
           path={mdiCartOutline}
@@ -27,26 +43,126 @@ const CartNavbarButton = () => {
       {shouldRenderCart && (
         <>
           <div
-            className='fixed right-0 top-0 h-screen w-full md:w-1/2 lg:w-1/4 bg-stone-50 pr-[3vw] py-6 z-[70]'
+            className='fixed right-0 px-[3vw] md:px-[1vw] flex flex-col justify-between text-stone-800 top-0 h-screen w-full md:w-1/2 lg:w-1/4 bg-white py-6 z-[70]'
             style={
               isCartOpened
                 ? { animation: 'inAnimationCart 500ms ease' }
                 : { animation: 'outAnimationCart 510ms ease' }
             }>
-            <button
-              type='button'
-              className='flex space-x-2 items-center justify-center ml-auto'
-              onClick={handleCartToggle}>
-              <Icon
-                path={mdiClose}
-                size={1}
-              />
-            </button>
+            <div>
+              <div className='flex items-center justify-between mb-4'>
+                <span className='text-xl'>Cart</span>
+                <button
+                  type='button'
+                  className='flex items-center justify-center ml-auto '
+                  onClick={() => setIsCartOpened(!isCartOpened)}>
+                  <Icon
+                    path={mdiClose}
+                    size={1}
+                  />
+                </button>
+              </div>
+              <div className='flex flex-col justify-between space-y-4'>
+                {carted.map((product: CartedProductType, index: number) => (
+                  <div
+                    key={product.id + index}
+                    className='flex shadow-md p-2 space-x-8 text-lg'>
+                    <Link
+                      onClick={() => setIsCartOpened(!isCartOpened)}
+                      href={`/${product.collection}/${product.id}?size=${product.size}&color=${product.color}`}>
+                      <Image
+                        src={product.image}
+                        alt={`Picture of ${
+                          product.title.charAt(0).toUpperCase() +
+                          product.title.slice(1)
+                        }`}
+                        width='100'
+                        height='100'
+                        quality={80}
+                      />
+                    </Link>
+                    <div className='w-full flex flex-col justify-between'>
+                      <div className='w-full flex justify-between items-center text-xl'>
+                        <h3>
+                          {product.title.charAt(0).toUpperCase() +
+                            product.title.slice(1)}
+                        </h3>
+                        <h3>€ {product.price}</h3>
+                      </div>
+                      <div className='flex justify-between items-center'>
+                        <div className='flex space-x-2 h-full'>
+                          <div>
+                            <button
+                              type='button'
+                              className='border border-stone-800 px-2'>
+                              -
+                            </button>
+                            <input
+                              type='number'
+                              value={product.quantity}
+                              className='border-y border-stone-800 px-2 w-6'
+                            />
+
+                            <button
+                              type='button'
+                              onClick={() => updateProductQuantity(product)}
+                              className='border border-stone-800 px-2'>
+                              +
+                            </button>
+                          </div>
+                          <div className='border border-stone-800 px-2 flex justify-center items-center'>
+                            {product.size.toUpperCase()}
+                          </div>
+                          <div className='h-full p-0.5 border border-stone-800'>
+                            <div
+                              className={`bg-${product.color}-400 h-full w-8 `}
+                            />
+                          </div>
+                        </div>
+                        <button
+                          type='button'
+                          onClick={() =>
+                            setCarted(
+                              carted.filter(
+                                (productToDelete: CartedProductType) =>
+                                  productToDelete.id != product.id
+                              )
+                            )
+                          }>
+                          <Icon
+                            path={mdiTrashCanOutline}
+                            size={1.1}
+                          />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className='w-full space-y-4 border-t border-stone-400 pt-4'>
+              <div className='flex justify-between'>
+                <span>Total</span>
+                <span>
+                  €{' '}
+                  {carted.reduce(
+                    (acc, product) => acc + product.price * product.quantity,
+                    0
+                  )}
+                </span>
+              </div>
+              <button
+                type='button'
+                className='text-lg w-full bg-stone-800 py-1 border-2 border-stone-800 text-stone-200 hover:text-stone-800 hover:bg-white transition-colors'>
+                Checkout
+              </button>
+            </div>
           </div>
           <div
             className='fixed top-0 left-0 right-0 bottom-0 bg-stone-800/80 z-[60] transition-all duration-500 opacity-0'
-            onClick={handleCartToggle}
-            style={isCartOpened ? { opacity: 1 } : { opacity: 0 }}></div>
+            onClick={() => setIsCartOpened(!isCartOpened)}
+            style={isCartOpened ? { opacity: 1 } : { opacity: 0 }}
+          />
         </>
       )}
     </>
