8c687d0ff03e6d5ca0ee6dcad8ad452dcb55983b
Michal Ciberej <michal.ciberej@gmail.com>
2023-10-08 00:04:48 +0200

    Added more pages

---
+++ app/women-autumn-2024/[id]/page.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Image from 'next/image';
+import getDatabaseDataById from '@/app/hooks/getDatabaseDataById';
+import ProductForm from '@/app/components/ProductForm';
+
+const ProductPage = async ({ params: { id } }: { params: { id: string } }) => {
+  const product = await getDatabaseDataById(id);
+  return (
+    <>
+      <main className='grid md:grid-cols-2'>
+        <div>
+          <ul className='flex flex-col'>
+            {product.images.map((img: string, index: number) => (
+              <li
+                className='lg:w-[50vw] h-screen flex justify-center items-center bg-gradient-to-br from-stone-200 to-stone-50'
+                key={index}>
+                <Image
+                  src={img}
+                  alt='Picture of product'
+                  width={500}
+                  height={500}
+                  quality={80}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ProductForm product={product} />
+      </main>
+      <div className='px-[3vw] md:px-[10vw] py-20'>{product.description}</div>
+    </>
+  );
+};
+
+export default ProductPage;
---
+++ app/women-autumn-2024/page.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Image from '@/node_modules/next/image';
+import ProductCard from '../components/ProductCard';
+import getDatabaseData from '../hooks/getDatabaseData';
+import { ProductType } from '@/typings';
+import Video from '../components/Video';
+
+const WomenSummer2024 = async () => {
+  const { documents } = await getDatabaseData();
+
+  return (
+    <main>
+      <section>
+        <div className='w-full h-[100dvh]'>
+          <Image
+            src={'/women-summer-2024-hero.png'}
+            alt='Picture of women wearing clothes from out summer 2024 collection'
+            width={1920}
+            height={1080}
+            className='h-full object-cover'
+          />
+        </div>
+      </section>
+      <section>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4'>
+          {documents.map((product: ProductType) => {
+            if (product.id.includes('WA24'))
+              return (
+                <ProductCard
+                  product={product}
+                  key={product.id}
+                />
+              );
+          })}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default WomenSummer2024;
+
+export const generateStaticParams = async () => {
+  const staticParams = await getDatabaseData();
+
+  return staticParams.documents.map((obj: ProductType) => ({ id: obj.id }));
+};
