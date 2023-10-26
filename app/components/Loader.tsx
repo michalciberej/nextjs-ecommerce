6ec4f64dd31a5560f21cd762272f4b418826aa6b
Michal Ciberej <michal.ciberej@gmail.com>
2023-10-26 15:59:20 +0200

    Added Image loader

---
+++ app/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className='flex justify-center items-center bg-white h-full w-full absolute inset-0 z-50 space-x-4'>
+      <div className='w-4 h-4 rounded-full bg-gray-900 animate-bounce duration' />
+    </div>
+  );
+};
+
+export default Loader;
--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,18 +1,25 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import { ProductType } from '@/typings';
+import Loader from './Loader';
+import { useState } from 'react';
 
 const colors = ['slate', 'blue', 'orange'];
 const bgColors = ['bg-slate-400', 'bg-blue-400', 'bg-orange-400'];
 
 const ProductCard = ({ product }: { product: ProductType }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <div className='flex flex-col border'>
       <Link
         href={`/women-${product.id.includes('WS') ? 'summer' : 'autumn'}-2024/${
           product.id
         }`}
-        className='flex justify-center items-center h-[400px] bg-gradient-to-tr from-stone-50 to-stone-200'>
+        className='flex justify-center items-center h-[400px] bg-gradient-to-tr from-stone-50 to-stone-200 relative'>
+        {!isLoaded && <Loader />}
         <Image
           src={product.images.at(0)}
           alt={`Picture of ${
@@ -21,6 +28,7 @@ const ProductCard = ({ product }: { product: ProductType }) => {
           width={250}
           height={250}
           quality={80}
+          onLoadingComplete={() => setIsLoaded(true)}
         />
       </Link>
       <div className='flex justify-between items-center px-4 my-2 text-lg'>
